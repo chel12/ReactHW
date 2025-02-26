b4bab09df833930250f25de537d13d030f3730de
Suboch. S <sema.chel12@gmail.com>
2025-02-26 16:47:17 +0300

    production-info 3/3

--- src/shared/components/Production-info.tsx
+++ src/shared/components/Production-info.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import { Banner } from './banners/Banner';
+import { Card } from './cards/Card';
 
 interface Props {
 	className?: string;
 }
-
+const services = [
+	{
+		id: '1',
+		img: '/public/img/arr1.png',
+		title: 'Разработка лекал и дизайна',
+		description:
+			'Поможем создать уникальные лекала для вашей одежды или адаптировать уже имеющиеся. Разработаем индивидуальный дизайн ваших товаров',
+	},
+	{
+		id: '2',
+		img: '/public/img/arr2.png',
+		title: 'Пошив одежды на заказ',
+		description:
+			'Пошив трикотажных изделий любой сложности и в любых объемах, как по вашим образцам, так и по нашим базовым лекалам',
+	},
+	{
+		id: '3',
+		img: '/public/img/arr3.png',
+		title: 'Бренд одежды под ключ',
+		description:
+			'Поможем разработать линейку одежды для вашего бренда, от пошива до реализации.',
+	},
+];
 export const ProductionInfo: React.FC<Props> = ({ className }) => {
 	return (
 		<section className="production-info">
@@ -26,9 +49,8 @@ export const ProductionInfo: React.FC<Props> = ({ className }) => {
 					<div className="production-info__inner-body">
 						<div className="production-info__title">
 							<h3>
-								Производство одежды 
-								для компаний, брендов,
-								 артистов
+								Производство одежды для компаний, брендов,
+								артистов
 							</h3>
 						</div>
 
@@ -46,7 +68,18 @@ export const ProductionInfo: React.FC<Props> = ({ className }) => {
 			</div>
 
 			<div className="production-info__footer">
-				<div className="container"></div>
+				<div className="container">
+					<div className="production-info__footer-inner">
+						{services.map((item) => (
+							<Card
+								key={item.id}
+								image={item.img}
+								title={item.title}
+								description={item.description}
+							/>
+						))}
+					</div>
+				</div>
 			</div>
 		</section>
 	);
